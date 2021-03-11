c75d5f31725f6157b024baf04d0af6abae9bec62
styrheim <styrheim@olivant.fo>
2021-03-11 14:36:41 +0000

    Added id for mover-1

--- src/components/scrollingbase.tsx
+++ src/components/scrollingbase.tsx
@@ -2,7 +2,7 @@ const Scrollingbase = (props:any) => {
 return(
 
   <div className="ground" id='ground'>
-  <div className="mover-1"></div>
+  <div id="mover-1" className="mover-1"></div>
  
 </div>
 )

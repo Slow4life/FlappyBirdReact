a78512d827fe5c9c5aa4fc1817d4754b5c878e30
styrheim <styrheim@olivant.fo>
2021-03-14 11:49:04 +0000

    React.createElement used, rather than JSX tags

--- src/components/MockRect.tsx
+++ src/components/MockRect.tsx
@@ -14,10 +14,25 @@ export class CollidingRectangles extends React.Component{
         new MockRect({id:"R8", height:20,width:20,left:90,top:90})
     ]
 
+
+
     render(){
-        let result = <div id="R0" title="R0" style={{position: "absolute",height:100,width:100,left:200,top:300}}>
-            R0
-        </div>
+        let result =
+            <div>
+                <div>intro</div>
+                <div id="R0" title="R0" style={{position: "absolute",height:100,width:100,left:200,top:300,border:"thin solid silver"}}>
+                R0
+                {React.createElement(MockRect,({id:"R1", height:10,width:100,left:-50,top:-50}))}
+                {React.createElement(MockRect,({id:"R2", height:10,width:200,left:-50,top:130}))}
+                {React.createElement(MockRect,({id:"R3", height:180,width:10,left:-30,top:-70}))}
+                {React.createElement(MockRect,({id:"R4", height:180,width:10,left:130,top:-20}))}
+                {React.createElement(MockRect,({id:"R5", height:50,width:50,left:25,top:25}))}
+                {React.createElement(MockRect,({id:"R6", height:20,width:20,left:90,top:-10}))}
+                {React.createElement(MockRect,({id:"R7", height:20,width:20,left:90,top:50}))}
+                {React.createElement(MockRect,({id:"R8", height:20,width:20,left:90,top:90}))}
+                </div>
+            </div>
+        //result.appendChild(<div>a child</div>)
         return(result)
     }
 }

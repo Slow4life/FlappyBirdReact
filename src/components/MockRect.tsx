69b8f34ea5b3989bf9bf7a42b0d6599b2f9d23cf
styrheim <styrheim@olivant.fo>
2021-03-14 20:43:00 +0000

    r0 should now also be available for testing

--- src/components/MockRect.tsx
+++ src/components/MockRect.tsx
@@ -31,6 +31,7 @@ export class MockRect extends React.Component<IMockRect>{
 export class CollidingRectangles extends React.Component{
     // To actually see these rectangles, and which of them collide:
     // Have e.g. App.tsx render CollidingRectangles
+    static readonly r0 = new MockRect({ id:"R0", height:100, width:100, left:0, top:0})
     static readonly r1 = new MockRect({ id:"R1", height:10, width:200, left:-50, top:-50})
     static readonly r2 = new MockRect({ id:"R2", height:10, width:200, left:-50, top:130})
     static readonly r3 = new MockRect({ id:"R3", height:180, width:10, left:-30, top:-70})
@@ -39,9 +40,9 @@ export class CollidingRectangles extends React.Component{
     static readonly r6 = new MockRect({ id:"R6", height:20, width:20, left:90, top:-10})
     static readonly r7 = new MockRect({ id:"R7", height:20, width:20, left:90, top:50})
     static readonly r8 = new MockRect({ id:"R8", height:20, width:20, left:90, top:90})
-    static readonly r0 =
-        <div id="R0" title="R0" style={{position: "absolute",height:100,width:100,left:75,top:250,border:"thin solid red"}}>
-            R0
+    static readonly r =
+        <div id="R0" title="R0" style={{position: "absolute",height:100,width:100,left:75,top:250,border:"none"}}>
+            {CollidingRectangles.r0.render()}
             {CollidingRectangles.r1.render()}
             {CollidingRectangles.r2.render()}
             {CollidingRectangles.r3.render()}
@@ -62,7 +63,7 @@ export class CollidingRectangles extends React.Component{
                 <p>Otherwise, these are the rectangles defined
                     in <i>MockRect.tsx</i> and used to test …() in <i>collision.test.tsx</i></p>
                 <p>If it is important to test if an exact touch is a collision, more cases should be added.</p>
-                {CollidingRectangles.r0}
+                {CollidingRectangles.r}
             </div>
         //result.appendChild(<div>a child</div>)
         return(result)

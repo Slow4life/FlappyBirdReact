a94bac9b9f5d295b35c46a5b41142e66d15839d0
Jónas Johannesen <jonasj@gmail.com>
2021-03-15 15:45:33 +0000

    changed variable names

--- src/gameEngine/Audio.ts
+++ src/gameEngine/Audio.ts
@@ -2,38 +2,38 @@
 /*class for individual sound effects
 */
 export class SoundEffect {
-    private _player: HTMLAudioElement;
+    private sound: HTMLAudioElement;
    
     /*creating the sound effect
     @param assetPath: path to the audio file.
     @param loop: if the file should loop or played once.
     */
     public constructor(assetPath: string, loop: boolean){
-        this._player = new Audio(assetPath);
-        this._player.loop = loop;
-        this._player.crossOrigin="anonymous";
+        this.sound = new Audio(assetPath);
+        this.sound.loop = loop;
+        this.sound.crossOrigin="anonymous";
     }
 
     //getter for the loop
     public get loop(): boolean{
-        return this._player.loop;
+        return this.sound.loop;
     }
 
     //setter for if the sound effect should loop or not
     public set loop(value: boolean){
-        this._player.loop = value;
+        this.sound.loop = value;
     }
 
     public play(): void {
-        if (!this._player.paused) {
+        if (!this.sound.paused) {
             this.stop();
         }
-        this._player.play();
+        this.sound.play();
     }
 
     public stop(): void {
-        this._player.pause();
-        this._player.currentTime = 0;
+        this.sound.pause();
+        this.sound.currentTime = 0;
     }
 }
 
@@ -41,7 +41,7 @@ export class SoundEffect {
 export class AudioManager{
 
     //holds all the loaded audio files
-    private static _soundEffects: {[name: string]: SoundEffect} = {};
+    private static soundArray: {[name: string]: SoundEffect} = {};
 
     /*
     loads the audio file
@@ -50,7 +50,7 @@ export class AudioManager{
     @param loop: if the file should loop or played once. 
     */
     public static loadAudioFile(name: string, audioPath: string, loop: boolean): void{
-        AudioManager._soundEffects[name] = new SoundEffect(audioPath, loop);
+        AudioManager.soundArray[name] = new SoundEffect(audioPath, loop);
     }
 
     /*
@@ -58,8 +58,8 @@ export class AudioManager{
     @param name: name of the audio file youn want to play.
     */
     public static playAudio(name: string): void {
-        if(AudioManager._soundEffects[name] !== undefined){
-            AudioManager._soundEffects[name].play();
+        if(AudioManager.soundArray[name] !== undefined){
+            AudioManager.soundArray[name].play();
         }
     }
 
@@ -68,8 +68,8 @@ export class AudioManager{
     @param name: name of the audio file you want to stop.
     */
     public static stopAudio(name: string): void {
-        if(AudioManager._soundEffects[name] !== undefined){
-            AudioManager._soundEffects[name].stop();
+        if(AudioManager.soundArray[name] !== undefined){
+            AudioManager.soundArray[name].stop();
         }
     }
 }
